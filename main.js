8ab0fe3db788d7ee604459d1fdeb09a60b70c455
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-12 17:25:42 -0400

    completed the login page

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.getElementById('submitBtn').onclick = console.log('hello');
+window.onload = function() { 
+    document.getElementById('loginForm').addEventListener('submit', function(event){
+        event.preventDefault();
+    var emailId = document.getElementById('emailInput').value;
+    var password = document.getElementById('passwordInput').value;
+    document.getElementById('submitBtn').onclick = console.log(`Email: ${emailId} Password: ${password}`);
+    });
+}
